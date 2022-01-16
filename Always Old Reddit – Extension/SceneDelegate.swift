f611934c8917c097654cf6359c588c61b9b3d5eb
Andrew Hill <contact@andrewthomashill.co.uk>
2022-01-16 19:36:06 +0100

    Initial Commit

---
+++ Always Old Reddit – Extension/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Always Old Reddit – Extension
+//
+//  Created by Andrew Hill on 16/01/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
